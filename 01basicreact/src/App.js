f3346bfe6e172be7c40cf677ce45b6f996c59c6b
RajRao01 <unofficialraj01@gmail.com>
2025-01-24 20:57:17 +0530

    basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I am Vishal Rao
+        </p>
+        <h1>I am a good developer!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
