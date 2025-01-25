961aa5218da9c72d890177e11d48e04cda6a7686
RajRao01 <unofficialraj01@gmail.com>
2025-01-25 08:25:58 +0530

    A basic vitereact created app

---
+++ 01vitereact/src/Vishal.jsx
@@ -0,0 +1,12 @@
+function Vishal() {
+
+    return(
+        <>
+        <h1> I am Vishal Rao</h1>
+        <p> I am a good Web Developer</p>
+        </>
+    
+    )
+}
+
+export default Vishal;
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Vishal from './vishal';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Vishal />
+  </StrictMode>,
+)
