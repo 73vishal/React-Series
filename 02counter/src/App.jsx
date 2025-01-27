3dfa969dafdf6b53adbc94dc6e5c8bd5aab25cbf
RajRao01 <unofficialraj01@gmail.com>
2025-01-27 09:00:21 +0530

    a projec use useState hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+  
+  // let counter = 15;
+
+  const addValue = () => {
+    console.log("clicked",counter);
+    // counter = counter + 1;
+    setCounter(counter + 1);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+  <h1> Vishal is a Good Web developer </h1>
+  <h2> Counter Value: {counter} </h2>
+
+  <button onClick={addValue}> Add value</button>
+  <br />
+  <button onClick={removeValue}> Remove value</button>
+    </>
+  )
+}
+
+export default App
