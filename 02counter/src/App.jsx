62e666fd726edd93e46947cf16e97d3cf37a0fe6
RajRao01 <unofficialraj01@gmail.com>
2025-01-29 20:26:35 +0530

    update the counter data

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,19 +11,26 @@ function App() {
     console.log("clicked",counter);
     // counter = counter + 1;
     setCounter(counter + 1);
-  }
+  //   setCounter(prevCounter => prevCounter + 1);
+  //   setCounter(prevCounter => prevCounter + 1);
+  //   setCounter(prevCounter => prevCounter + 1);
+  //   setCounter(prevCounter => prevCounter + 1);
+    }
   const removeValue = () => {
-    setCounter(counter - 1)
-  }
+    setCounter(counter - 1);
+  //   setCounter(prevCounter => prevCounter - 1);
+  //   setCounter(prevCounter => prevCounter - 1);
+  //   setCounter(prevCounter => prevCounter - 1);
+    }
 
   return (
     <>
   <h1> Vishal is a Good Web developer </h1>
   <h2> Counter Value: {counter} </h2>
 
-  <button onClick={addValue}> Add value</button>
+  <button  onClick={addValue}> Add value</button>
   <br />
-  <button onClick={removeValue}> Remove value</button>
+  <button  onClick={removeValue}> Remove value</button>
     </>
   )
 }
